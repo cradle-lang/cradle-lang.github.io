a3dae6f1a73534c137b25ed2c6a5a5651c60efcb
mashmllo <bryansim02@gmail.com>
2026-08-24 21:57:01 +0800

    Resolve landing page regressions

--- src/components/homepage/StartLearning/StartLearning.tsx
+++ src/components/homepage/StartLearning/StartLearning.tsx
@@ -42,7 +42,7 @@ export default function StartLearning(): ReactNode {
 
           <Link
             className={styles.learningCard}
-            to="/docs/overview/what-is-cradle">
+            to="docs/getting-started/first-scenario">
             <span className={styles.cardNumber}>03</span>
             <h3>Model a scenario</h3>
             <p>
